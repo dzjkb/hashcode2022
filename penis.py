cecd6552996570349bf887638ad5525d5715cc7b
dzjkb <jakub.pies@gmail.com>
2022-02-24 20:09:53 +0100

    FIX KURWA

--- penis.py
+++ penis.py
@@ -11,6 +11,16 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Projekt:
+    nazwa: str
+    dni: int
+    skor: int
+    best_bifor: int
+    ile_roli: int
+    skile: dict[str, int]
+
+
+@dataclass
 class Kontrykurwabutor:
     imie: str
     skille: dict[str, int]
@@ -21,6 +31,7 @@ class Dane:
     c: int
     p: int
     contrs: list[Kontrykurwabutor]
+    projekty: list[Projekt]
 
 
 def wez_sie(kurwa: str) -> Dane:
@@ -35,11 +46,23 @@ def wez_sie(kurwa: str) -> Dane:
         im, liczba_skili = lines[linia].split()
         linia += 1
         con = Kontrykurwabutor(im, dict())
-        for _ in liczba_skili:
+        for _ in range(int(liczba_skili)):
             skil, poziom = lines[linia].split()
             linia += 1
-            con.skille[skil] = poziom
+            con.skille[skil] = int(poziom)
 
         lsta_kontrow.append(con)
 
-    return Dane(c, p, lsta_kontrow)
+    lista_proj = []
+    for projekt_id in range(p):
+        im, czas, skor, bifordej, ilosc_roli = lines[linia].split()
+        linia += 1
+        proj =  Projekt(im, int(czas), int(skor), int(bifordej), int(ilosc_roli), dict())
+        for _ in range(int(ilosc_roli)):
+            skil, level = lines[linia].split()
+            proj.skile[skil] = int(level)
+            linia += 1
+
+        lista_proj.append(proj)
+
+    return Dane(c, p, lsta_kontrow, lista_proj)
