6725b7d499ccf983c54d94032175c35a4291c5a3
dzjkb <jakub.pies@gmail.com>
2022-02-24 21:22:51 +0100

    SKORER CO?!

--- testerka.py
+++ testerka.py
@@ -1,25 +1,42 @@
 from penis import Dane
 
 
+def aa(dane: Dane):
+    eee = dict()
+    for p in dane.projekty:
+        eee[p.nazwa] = p
+
+    return eee
+
+
 def skoruj(projekt_tu_kontrybutors: dict[str, list[str]], dane: Dane):
+    projektorsy = aa(dane)
     kontr_to_dzien = dict()
-    kontrybutor_tu_projekts = dict()
+    # kontrybutor_tu_projekts = dict()
     for p, kontrs in projekt_tu_kontrybutors.items():
         for k in kontrs:
-            if k in kontrybutor_tu_projekts:
-                kontrybutor_tu_projekts[k].append(p)
-            else:
-                kontrybutor_tu_projekts[k] = [p]
+        #     if k in kontrybutor_tu_projekts:
+        #         kontrybutor_tu_projekts[k].append(p)
+        #     else:
+        #         kontrybutor_tu_projekts[k] = [p]
 
             kontr_to_dzien[k] = 0
 
     pr_tu_dzienzakonczenia = dict()
     for p, kontrs in projekt_tu_kontrybutors.items():
-        dziendostepnosci = max([k.dzien for k in kontrs])
-        pr_tu_dzienzakonczenia[p] = dziendostepnosci
+        dziendostepnosci = max([kontr_to_dzien[k] for k in kontrs])
+        dzienzakonczenia = dziendostepnosci + projektorsy[p].dni
+        pr_tu_dzienzakonczenia[p] = dzienzakonczenia
 
         for k in kontrs:
-            kontr_to_dzien[k] += 0
+            kontr_to_dzien[k] = dzienzakonczenia
+
+    SKOR_KURWA = 0
+    for p in pr_tu_dzienzakonczenia:
+        pskor = max(0, projektorsy[p].skor - (pr_tu_dzienzakonczenia[p] - projektorsy[p].best_bifor))
+        SKOR_KURWA += pskor
+
+    return SKOR_KURWA
 
 
 def aut(ptk: dict[str, list[str]], sciezka: str):
