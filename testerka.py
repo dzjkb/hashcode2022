c30c8fcac7d75387a76c6003a63cc3f1a15c50a3
dzjkb <jakub.pies@gmail.com>
2022-02-24 21:06:45 +0100

    EEEEEEEEEEEEEEEEE

--- testerka.py
+++ testerka.py
@@ -1,15 +1,4 @@
-from dataclasses import dataclass
-
-from .penis import Dane
-
-
-@dataclass
-class Kontr:
-    im: str
-    dzien: int
-
-    def dodajdz(self, d: int):
-        return Kontr(self.im, self.dzien + d)
+from penis import Dane
 
 
 def skoruj(projekt_tu_kontrybutors: dict[str, list[str]], dane: Dane):
@@ -25,17 +14,19 @@ def skoruj(projekt_tu_kontrybutors: dict[str, list[str]], dane: Dane):
             kontr_to_dzien[k] = 0
 
     pr_tu_dzienzakonczenia = dict()
-
     for p, kontrs in projekt_tu_kontrybutors.items():
         dziendostepnosci = max([k.dzien for k in kontrs])
         pr_tu_dzienzakonczenia[p] = dziendostepnosci
 
+        for k in kontrs:
+            kontr_to_dzien[k] += 0
+
 
 def aut(ptk: dict[str, list[str]], sciezka: str):
     with open(sciezka, 'w') as f:
-        f.write(len(ptk))
+        f.write(f"{str(len(ptk))}\n")
         for p, kontrs in ptk.items():
-            f.write(p)
-            f.write(" ".join(kontrs))
+            f.write(f"{p}\n")
+            f.write(" ".join(kontrs) + "\n")
 
-    f.write()
+        f.write("\n")
